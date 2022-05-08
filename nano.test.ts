6d81540d32a56ee740cd8ab7b81618d15dd13bd2
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-08 18:05:58 +0200

    Keep test file

--- nano.test.ts
+++ nano.test.ts
@@ -1,47 +1,25 @@
 import { scan, parse, compile, render } from './mod.ts';
 
 const TEST_METHODS = {
-	upper: (value: any) => value.toString().toUpperCase(),
-	more_than_three: (value: any) => value > 3,
+	what: (v: any) => typeof v
 };
 
 const TEST_DATA = {
-	'000_technically_invalid': 'lol',
-	string: 'hello_from_data',
-	number: 200,
-	an_array: ['banana', 'apple', 'pear'],
 	an_object: { a: 'banana', b: 'apple', c: 'pear' },
 };
 
 const TEST_INPUT = `
-	<div>Hei</div>
-	<div>{{ number }}</div>
-	<div>{{ an_object.a }}</div>
-	<div>{{ "Literally this string" }}</div>
-	{% for key, value in an_object %}
-		<section>
-			<div>{{ key }}: {{ value }}</div>
-		</section>
-	{% endfor %}
-
-	{% if number %}
-		<div>{{ number}}</div>
+	{% if not_here %}
+		IF
+	{% else %}
+		{{ not_here | what }}
+		ELSE
 	{% endif %}
-
-	{% for item in an_array %}
-		<div>{{ item }}</div>
-	{% endfor %}
 `;
 
 try {
 	console.time('RENDER');
-
-	// const scanned = scan(TEST_INPUT);
-	// const parsed = parse(scanned);
-	// const compiled = await compile(parsed, TEST_DATA, TEST_METHODS);
-	const rendered = await render(TEST_INPUT, TEST_DATA, TEST_METHODS);
-
-	console.log(rendered);
+	console.log(await render(TEST_INPUT, TEST_DATA, TEST_METHODS));
 	console.timeEnd('RENDER');
 } catch (error) {
 	console.log('%c' + error.message, 'color: red');
