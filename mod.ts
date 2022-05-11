16bf8b61c9fbfaa84c0fd4b2fe2a8d7bab8c0bae
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-11 20:40:17 +0200

    Add check for empty strings

--- mod.ts
+++ mod.ts
@@ -257,6 +257,7 @@ export function parse(marks: Mark[]): Node[] {
 	const RE_ACCESS_DOT = /\./;
 	const RE_ACCESS_BRACKET = /\[["']|['"]\]/;
 	const RE_VARIABLE_EXPRESSION_LIKE = /[\&\|\<\>\+\-\=\!\{\}\,]/;
+	const RE_VARIABLE_EMPTY = /^['"]['"]$/;
 	const RE_VARIABLE_IN_QUOTES = /^['"].+?['"]$/;
 	const RE_VARIABLE_BRACKET_NOTATION = /\[['"]/;
 	const RE_VARIABLE_DIGIT = /^-?(\d|\.\d)+$/;
@@ -309,7 +310,7 @@ export function parse(marks: Mark[]): Node[] {
 		const variable_parts = value_string.split(RE_ACCESS_DOT);
 
 		for (const part of variable_parts) {
-			if (!RE_VARIABLE_VALID.test(part)) {
+			if (!RE_VARIABLE_EMPTY.test(part) && !RE_VARIABLE_VALID.test(part)) {
 				throw new NanoError(`Invalid variable name: "${value_string}"`);
 			}
 		}
