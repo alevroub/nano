46803015083cd780c05220a3179b2af534d6cfda
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-11 19:11:51 +0200

    Keep test file

--- nano.test.ts
+++ nano.test.ts
@@ -5,21 +5,69 @@ const TEST_METHODS = {
 };
 
 const TEST_DATA = {
-	an_object: { a: 'banana', b: 'apple', c: 'pear' },
+	things: { a: 'banana', b: 'apple', c: 'pear' },
+	A: false,
+	C: true
 };
 
-const TEST_INPUT = `
-	{% if not_here %}
-		IF
-	{% else %}
-		{{ not_here | what }}
-		ELSE
-	{% endif %}
-`;
+const TEST_INPUT = [
+	'{% if A %}',
+		'{% if AA %}',
+			'{% for n in NN %}',
+				'NNN',
+			'{% endfor %}',
+		'{% else %}',
+			'{% if AAA %}',
+				'AAA',
+			'{% else %}',
+				'AB',
+			'{% endif %}',
+		'{% endif %}',
+	'{% else %}',
+		'{% if B %}',
+			'B',
+		'{% else %}',
+			'{% if C %}',
+				'C',
+			'{% else %}',
+				'NOT C',
+			'{% endif %}',
+		'{% endif %}',
+	'{% endif %}',
+].join('');
+
+
+const TEST_INPUT_B = [
+	'{% if A %}',
+		'{% if AA %}',
+			'{% for n in NN %}',
+				'NNN',
+			'{% endfor %}',
+		'{% elseif AAA %}',
+			'AAA',
+		'{% else %}',
+			'AB',
+		'{% endif %}',
+	'{% elseif B %}',
+		'B',
+	'{% elseif C %}',
+		'C',
+	'{% else %}',
+		'NOT C',
+	'{% endif %}',
+].join('');
 
 try {
 	console.time('RENDER');
+
+	const a = JSON.stringify(scan(TEST_INPUT), null, 3);
+	const b = JSON.stringify(scan(TEST_INPUT_B), null, 3);
+
+	console.log(a === b);
+
 	console.log(await render(TEST_INPUT, TEST_DATA, TEST_METHODS));
+	console.log(await render(TEST_INPUT_B, TEST_DATA, TEST_METHODS));
+	// console.log(await compile(TEST_INPUT));
 	console.timeEnd('RENDER');
 } catch (error) {
 	console.log('%c' + error.message, 'color: red');
