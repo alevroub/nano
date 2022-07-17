f77210871ccdcf2b7ee710484e1faff81f187309
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-07-17 17:26:19 +0200

    Update doc

--- mod.ts
+++ mod.ts
@@ -212,16 +212,16 @@ export function scan(input: string): Mark[] {
  * 	care of syntax formatting and should provide all relevant properties
  * 	to the renderer.
  *
- * 	|	0	value_primitive       		"text" | 100 | true | false
- * 	|	1	value_variable        		variable.dot.separated / variable['named-key']
- * 	|	2	expression_filter     		variable | filter_1 | filter_2
- * 	|	3	expression_ternary    		variable ? 'value_if_true' : 'value_if_false'
- * 	|	4	expression_logical    		A && B || C
- * 	|	5	expression_unary      		!A
- * 	|	6	expression_binary     		== != > < >= <=
- * 	|	7	block_if              		{% if condition_1 %} {% elseif condition_2 %} {% endif %}
- * 	|	8	block_for             		{% for num, index in numbers | unique %} {% endfor %}
- * 	|	9	tag_comment           		{# multi-line comment #}
+ * 	|	0 	value_primitive       		"text" | 100 | true | false
+ * 	|	1 	value_variable        		variable.dot.separated / variable['named-key']
+ * 	|	2 	expression_filter     		variable | filter_1 | filter_2
+ * 	|	3 	expression_ternary    		variable ? 'value_if_true' : 'value_if_false'
+ * 	|	4 	expression_logical    		A && B || C
+ * 	|	5 	expression_unary      		!A
+ * 	|	6 	expression_binary     		== != > < >= <=
+ * 	|	7 	block_if              		{% if condition_1 %} {% elseif condition_2 %} {% endif %}
+ * 	|	8 	block_for             		{% for num, index in numbers | unique %} {% endfor %}
+ * 	|	9 	tag_comment           		{# multi-line comment #}
  * 	|	10	tag_import            		{{ import 'path/to/file.html' with { name: value } }}
  *
  **/
